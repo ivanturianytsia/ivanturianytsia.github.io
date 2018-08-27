fdfd54a997697a439c331cb5e9a4aa49869e3972
ivanturianytsia <vanyaturianitsa@gmail.com>
2018-08-27 23:58:08 +0300

    body transition

--- src/index.js
+++ src/index.js
@@ -23,12 +23,14 @@ const icons = [
 	'#codepen'
 ]
 
+// Config
 const maxvy = 10 // Max y-velocity
 const size = 40 // Width and height of elements
 const g = .1 // Gravity
 
-const svg = document.querySelector('#falling-skills')
-const vb = svg.getAttribute('viewBox').split(' ')
+let svg
+let vb
+let items
 
 const getInitialPosition = () => {
 	const r = Math.random()
@@ -40,20 +42,6 @@ const getInitialPosition = () => {
 		fill: `rgba(0, 0, 0, ${r * 0.2})`
 	}
 }
-
-let items = icons.map(item => {
-	// <image x="10" y="20" width="80" height="80" xlink:href="image.svg" />
-	const { x, y, vx, vy, vr, fill } = getInitialPosition()
-	const el = document.createElementNS('http://www.w3.org/2000/svg', 'use')
-	el.setAttributeNS(null, 'x', x)
-	el.setAttributeNS(null, 'y', y)
-	el.setAttributeNS(null, 'width', size)
-	el.setAttributeNS(null, 'height', size)
-	el.setAttributeNS(null, 'fill', fill)
-	el.setAttributeNS('http://www.w3.org/1999/xlink', 'href', item)
-	svg.appendChild(el)
-	return { el, x, y, vx, vy, vr, fill, a: 0 }
-})
 
 const update = function() {
 	items.forEach(item => {
@@ -79,4 +67,27 @@ const update = function() {
 	requestAnimationFrame(update)
 }
 
-update()
+const init = function () {
+	svg = document.querySelector('#falling-skills')
+	vb = svg.getAttribute('viewBox').split(' ')
+	items = icons.map(item => {
+		// <image x="10" y="20" width="80" height="80" xlink:href="image.svg" />
+		const { x, y, vx, vy, vr, fill } = getInitialPosition()
+		const el = document.createElementNS('http://www.w3.org/2000/svg', 'use')
+		el.setAttributeNS(null, 'x', x)
+		el.setAttributeNS(null, 'y', y)
+		el.setAttributeNS(null, 'width', size)
+		el.setAttributeNS(null, 'height', size)
+		el.setAttributeNS(null, 'fill', fill)
+		el.setAttributeNS('http://www.w3.org/1999/xlink', 'href', item)
+		svg.appendChild(el)
+		return { el, x, y, vx, vy, vr, fill, a: 0 }
+	})
+
+	update()
+}
+
+window.addEventListener('load', () => {
+	document.body.style.opacity = '1' // Full-page transition
+	init()
+})
